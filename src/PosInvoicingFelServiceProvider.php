aae2dcd3e136dfdf198b2ca0d1b80beafadb940c
alvaro-ipx <remberto.molina@ipxserver.com>
2022-02-22 09:50:41 -0400

    Initial package config

---
+++ src/PosInvoicingFelServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace EmizorIpx\PosInvoicingFel;
+
+use Illuminate\Support\ServiceProvider;
+
+class PosInvoicingFelServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
