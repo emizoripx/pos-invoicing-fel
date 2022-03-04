2e3764fe453074a8cb4efdb6c0bf7a602f769a5e
alvaro-ipx <remberto.molina@ipxserver.com>
2022-03-04 15:21:03 -0400

    Minor Fixes

--- src/Resources/Views/invoices/partials/laststatus.blade.php
+++ src/Resources/Views/invoices/partials/laststatus.blade.php
@@ -1,7 +1,7 @@
 <?php
 $badgeTypes=['badge-primary','badge-primary','badge-success','badge-success','badge-default','badge-warning','badge-success','badge-info','badge-danger','badge-success','badge-success','badge-success','badge-danger','badge-success','badge-success'];
-$tipoEstado = $invoice->tipoEmision == 'VALIDA' ? 6:8;
+$tipoEstado = $invoice->estado == 'VALIDA' ? 6:8;
 ?>
-@if($invoice->tipoEmision)
+@if($invoice->estado)
     <span class="badge {{ $badgeTypes[$tipoEstado] }} badge-pill">{{ __($invoice->estado) }}</span>
 @endif  
